5d109a1725ce611c19bf6299429df9f6cd7d274e
Your NameSpymamah <ikechukwumamah1@gmail.com>
2024-12-25 03:24:29 +0100

    first seaborn project

---
+++ seaborne.py
@@ -0,0 +1,20 @@
+import seaborn as sns
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+
+tips = sns.load_dataset('tips')
+
+print(tips.head())
+# visualization of data
+sns.relplot(data=tips, x = 'total_bill', y = 'tip', col = 'time', hue = 'smoker', style = 'smoker', size = 'size')
+plt.show()
+
+# import seaborn as sns
+# import matplotlib.pyplot as plt
+
+# sns.set(style="darkgrid")
+# tips = sns.load_dataset("tips")
+# sns.scatterplot(data=tips, x="total_bill", y="tip", hue="time")
+# plt.show()
+
